98329250233b70432430034fc860d836785e1bf9
モコクリ松口 <youichi@mococreations.com>
2018-12-20 08:59:35 +0900

    PCトップ完成

---
+++ js/common.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+//画像切り替え
+$(function() {
+  // 置換の対象とするclass属性。
+  var $elem = $('.image-switch');
+  // 置換の対象とするsrc属性の末尾の文字列。
+  var sp = '_sp.';
+  var pc = '_pc.';
+  // 画像を切り替えるウィンドウサイズ。
+  var replaceWidth = 640;
+
+  function imageSwitch() {
+    // ウィンドウサイズを取得する。
+    var windowWidth = parseInt($(window).width());
+
+    // ページ内にあるすべての`.image-switch`に適応される。
+    $elem.each(function() {
+      var $this = $(this);
+      // ウィンドウサイズが640px以上であれば_spを_pcに置換する。
+      if(windowWidth >= replaceWidth) {
+        $this.attr('src', $this.attr('src').replace(sp, pc));
+
+      // ウィンドウサイズが640px未満であれば_pcを_spに置換する。
+      } else {
+        $this.attr('src', $this.attr('src').replace(pc, sp));
+      }
+    });
+  }
+  imageSwitch();
+
+  // 動的なリサイズは操作後0.2秒経ってから処理を実行する。
+  var resizeTimer;
+  $(window).on('resize', function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      imageSwitch();
+    }, 100);
+  });
+    
+});
